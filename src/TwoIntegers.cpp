c3aac9759f6ca7c1da269cad58a50b9ddd9aae29
icossinib <icossinib@gmail.com>
2013-09-24 22:26:51 +0200

    Unstage CPPTest.cpp y a ver que pasa. despues commit

---
+++ src/TwoIntegers.cpp
@@ -0,0 +1,24 @@
+/*
+ * TwoIntegers.cpp
+ *
+ *  Created on: 24/09/2013
+ *      Author: "Ignacio Esteban Cossini Benchimol"
+ */
+
+#include "TwoIntegers.h"
+
+namespace KoalaJump
+{
+
+TwoIntegers::TwoIntegers()
+{
+	// TODO Auto-generated constructor stub
+
+}
+
+TwoIntegers::~TwoIntegers()
+{
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace KoalaJump */
---
+++ src/TwoIntegers.h
@@ -0,0 +1,25 @@
+/*
+ * TwoIntegers.h
+ *
+ *  Created on: 24/09/2013
+ *      Author: "Ignacio Esteban Cossini Benchimol"
+ */
+
+#ifndef TWOINTEGERS_H_
+#define TWOINTEGERS_H_
+
+namespace KoalaJump
+{
+
+/*
+ *
+ */
+class TwoIntegers
+{
+public:
+	TwoIntegers();
+	virtual ~TwoIntegers();
+};
+
+} /* namespace KoalaJump */
+#endif /* TWOINTEGERS_H_ */
